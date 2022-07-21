6f64a28e4a433309e90584a134c62b61ad988605
Aryaman Raj <aryaman0816@gmail.com>
2022-07-22 02:37:20 +0530

    what

--- inbox/compile.js
+++ inbox/compile.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
-
 const source = fs.readFileSync(inboxPath, 'utf8');
-console.Consolelog(solc.compile(source,1));
+
+//module.exports = solc.compile(source,1); because only one contracts file
+
+module.exports = solc.compile(source,1).contracts[':inbox'];
---
+++ inbox/test/inbox.test.js
@@ -0,0 +1,4 @@
+const asser = require('assert');
+const ganache = require('ganache-cle');
+const Web3 = require('web3');
+
